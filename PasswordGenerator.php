9d6536e9d7185255c0fb552e4b825c15bbda54f0
RasmuS <rasburkhanov@yandex.ru>
2026-06-30 08:17:37 +0000

    Fixed main

--- PasswordGenerator.php
+++ PasswordGenerator.php
@@ -53,3 +53,67 @@ function nextRandom(int $number): int
 {
     return (16807 * $number) % 2147483647;
 }
+
+
+function checkPassword(string $password): string
+{
+    $score = 0;
+    
+    // 1. Длина не меньше 8 символов
+    if (strlen($password) >= 8) {
+        $score++;
+    }
+    
+    // Разбиваем строку на символы для проверки
+    $chars = str_split($password);
+    $hasLowercase = false;
+    $hasUppercase = false;
+    $hasDigit = false;
+    $hasSpecial = false;
+    
+    foreach ($chars as $char) {
+        // 2. Есть строчная латинская буква
+        if ($char >= 'a' && $char <= 'z') {
+            $hasLowercase = true;
+        }
+        // 3. Есть ПРОПИСНАЯ латинская буква
+        elseif ($char >= 'A' && $char <= 'Z') {
+            $hasUppercase = true;
+        }
+        // 4. Есть цифра
+        elseif ($char >= '0' && $char <= '9') {
+            $hasDigit = true;
+        }
+        // 5. Есть спецсимвол
+        elseif (str_contains(SPECIAL, $char)) {
+            $hasSpecial = true;
+        }
+    }
+    
+    // Добавляем баллы за найденные признаки
+    if ($hasLowercase) {
+        $score++;
+    }
+    if ($hasUppercase) {
+        $score++;
+    }
+    if ($hasDigit) {
+        $score++;
+    }
+    if ($hasSpecial) {
+        $score++;
+    }
+    
+    // Определяем вердикт
+    if ($score <= 2) {
+        $verdict = 'Слабый';
+    } elseif ($score === 3) {
+        $verdict = 'Средний';
+    } elseif ($score === 4) {
+        $verdict = 'Надёжный';
+    } else {
+        $verdict = 'Очень надёжный';
+    }
+    
+    return "{$verdict} пароль (оценка {$score} из 5)";
+}
--- main.php
+++ main.php
@@ -3,14 +3,19 @@
 require_once __DIR__ . '/PasswordGenerator.php';
 
 use function hexlet\code\generatePassword;
+use function hexlet\code\checkPassword;
 
-// Готовый помощник: печатает значение и переносит строку.
 function println(string $value): void
 {
     echo $value . PHP_EOL;
 }
 
-println(generatePassword(8, 1));
-println(generatePassword(12, 123));
-println(generatePassword(12, 123, useSpecial: true));
-println(generatePassword(8, 1, useUppercase: false, useDigits: false));
+println('== Генерация паролей ==');
+println('буквы и цифры:    ' . generatePassword(12, 123));
+println('со спецсимволами: ' . generatePassword(16, 7, useSpecial: true));
+
+println('');
+println('== Проверка надёжности ==');
+println('abc        -> ' . checkPassword('abc'));
+println('abcdef1234 -> ' . checkPassword('abcdef1234'));
+println('Abcdef123! -> ' . checkPassword('Abcdef123!'));
